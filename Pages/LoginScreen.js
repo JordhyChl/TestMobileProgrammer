3f347956423e2054f7ccc6886408d22f310da1a5
JordhyChl <jordhy.a@gmail.com>
2020-10-28 19:56:51 +0700

    Test Async Storage

--- Pages/LoginScreen.js
+++ Pages/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, SafeAreaView, Alert, Switch } from 'react-native';
+import { View, Text, TouchableOpacity, SafeAreaView, Alert, StyleSheet } from 'react-native';
 import FloatingLabel from 'react-native-floating-labels';
 // import Mybutton from './components/Mybutton';
 //import Mytext from './components/Mytext';
@@ -7,6 +7,7 @@ import SQLite from 'react-native-sqlite-storage';
 import Mytextinput from './components/Mytextinput';
 import PwdInput from './components/PwdInput';
 import Mybutton from './components/Mybutton';
+import AsyncStorage from '@react-native-community/async-storage';
 import MyView from './MyView';
 
 let db;
@@ -16,7 +17,48 @@ export default class Login extends Component {
         this.state = {
             username: '',
             password: '',
+            textUsername: '',
+            textPassword: '',
         };
+        AsyncStorage.getItem('DataLogin', (error, result) => {
+            if (result) {
+                let resultParsed = JSON.parse(result)
+                this.setState({
+                    username: resultParsed.username,
+                    password: resultParsed.password
+                });
+            }
+        });
+    }
+
+    saveToLocal() {
+        let username = this.state.textUsername;
+        let password = this.state.textPassword;
+        let data = {
+            username: username,
+            password: password
+        }
+
+        AsyncStorage.setItem('DataLogin', JSON.stringify(data));
+
+        this.setState({
+            username: username,
+            password: password
+        });
+
+        alert('Data tersimpan');
+    }
+
+    async removeItemValue() {
+        try {
+            // Mengahpus data kdari local storage
+            await AsyncStorage.removeItem('DataLogin');
+            alert('Berhasil Menghapus Data')
+        }
+        catch (exception) {
+            console.log(exception)
+            alert('Gagal Menghapus Data')
+        }
     }
 
     componentDidMount() {
@@ -83,6 +125,13 @@ export default class Login extends Component {
             <SafeAreaView style={{ flex: 1 }}>
                 <View style={{ paddingTop: 200 }}>
                     {/* <Text>Login Information</Text> */}
+                    <Text style={styles.textWho}>
+                        Siapa Saya ?
+                </Text>
+                    <Text style={styles.viewResult}>
+                        Username: {this.state.username}{'\n'}
+                        Password: {this.state.password}
+                    </Text>
                     <Mytextinput
                         placeholder="Username"
                         onChangeText={(text) => this.setState({ username: text })}
@@ -99,8 +148,8 @@ export default class Login extends Component {
                     <Mybutton title="Login" customClick={() => this.onLoginPress()} />
                     <TouchableOpacity onPress={() => this.props.navigation.navigate('RegisterScreen')}>
                         <Text style={{ fontSize: 16, paddingTop: 20, textAlign: 'center', color: 'grey' }}>Register?</Text>
-                    </TouchableOpacity>             
-                    
+                    </TouchableOpacity>
+
                     <TouchableOpacity onPress={() => this.props.navigation.navigate('AdminScreen')}>
                         <Text style={{ fontSize: 16, paddingTop: 20, textAlign: 'center', color: 'grey' }}>View All Admin</Text>
                     </TouchableOpacity>
@@ -112,3 +161,36 @@ export default class Login extends Component {
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+        padding: 16,
+        paddingTop: 32
+    },
+    textWho: {
+        fontWeight: 'bold',
+        fontSize: 16
+    },
+    viewResult: {
+        flexDirection: 'row'
+    },
+
+    textInput: {
+        height: 40,
+        width: 300,
+        backgroundColor: 'white',
+        marginTop: 8,
+        marginBottom: 8,
+        borderWidth: 1,
+        borderColor: '#ddd',
+        padding: 8,
+        borderRadius: 10
+    },
+    viewRemove: {
+        marginTop: 20
+    }
+});
